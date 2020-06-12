abe0af238c77fc397290b4e066631ae9dffe9c52
DF-Mephisto <mephist04@mail.ru>
2020-06-12 11:10:06 +0300

    added topic creation

--- src/main/java/forum/entity/Comment.java
+++ src/main/java/forum/entity/Comment.java
@@ -13,23 +13,27 @@ import java.util.Date;
 @Data
 @Entity
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@Table(name="topic", schema = "public")
-public class Topic {
+@Table(name="comment", schema = "public")
+public class Comment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
     @NotBlank(message = "Can't be empty")
-    @Size(min=1, max=100, message = "Topic name must be between 1 and 100 length long")
-    String name;
+    @Size(min=1, max=1000, message = "Comment must be between 1 and 1000 length long")
+    String text;
 
     @Column(name="placedat")
     Date placedAt;
 
     @ManyToOne
-    @JoinColumn(name = "section_id")
-    Section section;
+    @JoinColumn(name = "topic_id")
+    Topic topic;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
 
     @PrePersist
     public void createdAt()
--- src/main/java/forum/web/SectionController.java
+++ src/main/java/forum/web/SectionController.java
@@ -1,9 +1,11 @@
 package forum.web;
 
+import forum.entity.Comment;
 import forum.entity.Section;
 import forum.entity.Topic;
 import forum.entity.User;
 import forum.forms.CreateTopicForm;
+import forum.repository.CommentRepository;
 import forum.repository.TopicRepository;
 import forum.service.Properties;
 import forum.repository.SectionRepository;
@@ -14,8 +16,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,15 +29,18 @@ public class SectionController {
 
     private SectionRepository secRepo;
     private TopicRepository topicRepo;
+    private CommentRepository comRepo;
     private Properties props;
 
     @Autowired
     public SectionController(SectionRepository secRepo,
                              TopicRepository topicRepo,
+                             CommentRepository comRepo,
                              Properties props)
     {
         this.secRepo = secRepo;
         this.topicRepo = topicRepo;
+        this.comRepo = comRepo;
         this.props = props;
     }
 
@@ -64,4 +71,40 @@ public class SectionController {
         return "section";
     }
 
+    @PostMapping("/{id}")
+    public String processTopicCreation(@PathVariable("id") Long id,
+                                       @Valid @ModelAttribute("newTopic") CreateTopicForm form,
+                                       BindingResult errors,
+                                       @RequestParam(value = "page", defaultValue = "0") int page,
+                                       @AuthenticationPrincipal User user,
+                                       Model model)
+    {
+        Optional<Section> secRes = secRepo.findById(id);
+        if (secRes.isEmpty()) return "redirect:/";
+        Section section = secRes.get();
+
+        if (errors.hasErrors())
+        {
+            Pageable pageable = PageRequest.of(page, props.getTopicsCount(),
+                    Sort.by(Sort.Direction.ASC, "placedAt"));
+            List<Topic> topics = topicRepo.findBySection_Id(id, pageable);
+            model.addAttribute("section", section);
+            model.addAttribute("topics", topics);
+            return "section";
+        }
+
+        Topic topic = new Topic();
+        topic.setName(form.getName());
+        topic.setSection(section);
+        topicRepo.save(topic);
+
+        Comment comment = new Comment();
+        comment.setText(form.getText());
+        comment.setTopic(topic);
+        comment.setUser(user);
+        comRepo.save(comment);
+
+        String redirectUrl = "section/" + id + "?page=" + page;
+        return "redirect:/" + redirectUrl;
+    }
 }
